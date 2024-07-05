b533d927c866393e79b0076aa8c7919208ad609b
Andromeddda <andrey.shik2005@gmail.com>
2024-07-05 15:56:23 +0500

    Rename cursed variable

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
 #include "hash_queue.hpp"
 
 int main(void) {
-	HashQueue<int> hui = HashQueue<int>(3);
-	hui.insert(1);
-	hui.insert(2);
-	hui.insert(3);
-	hui.insert(1);
-	hui.display();
-	hui.insert(4);
-	hui.insert(5);
-	hui.display();
+	HashQueue<int> hash_q = HashQueue<int>(4);
+	hash_q.insert(1);
+	hash_q.insert(2);
+	hash_q.insert(3);
+	hash_q.insert(1);
+	hash_q.display();
+	hash_q.insert(4);
+	hash_q.insert(5);
+	hash_q.display();
 	return 0;
 }
